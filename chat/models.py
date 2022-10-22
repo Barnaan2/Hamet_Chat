20cb837ee46dbab0265d59adf6c281f6214fd2ba
Barnaan2 <barnaantekalign@gmail.com>
2022-10-22 14:04:36 -0700

    model for chat app is added

--- chat/models.py
+++ chat/models.py
@@ -1,4 +1,26 @@
 from django.db import models
+from account.models import User
 
+class Chat(models.Model):
+    initiator = models.ForeignKey(User,on_delete = models.CASCADE)
+    reactor = models.ForeignKey(User,on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ('-created', '-updated')
 
-# Create your models here.
+    def __str__(self):
+        return self.initiator.username
+
+class Message(models.Model):
+   chat = models.ForeignKey(Chat,on_delete = models.CASCADE)
+   sender = models.ForeignKey(User,on_delete = models.CASCADE)
+   receiver = models.ForeignKey(User,on_delete = models.CASCADE)
+   body = models.TextField(max_length=1000)
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+   
+   class Meta:
+        ordering = ('-created', '-updated')
+   def __str__(self):
+        return self.sender.username
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,23 @@
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from . models import Chat,Message
+from account.models import User
 
 # Create your views here.
 def index(request):
+
     return render(request,"chat/index.html")
+
+
+def chat(request):
+    chat = Chat.objects.filter(initiator = request.user)
+    q = request.GET.get("q")
+    if q != None:
+      user = User.objects.get(id=q)
+      if user is None:
+         return HttpResponse("user you searching for, does not exist")
+      else:
+        Chat.objects.create(initiator = request.user, reactor =user)
+    context = {'chat': chat}
+    return render(request,'chat/your_chat.html',context)
