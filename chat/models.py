8c0ad5bf7934abae92ace92baa47d58910bbd9d6
Barnaan2 <barnaantekalign@gmail.com>
2022-10-23 13:04:06 -0700

    basic models are added for messaging

--- chat/models.py
+++ chat/models.py
@@ -3,11 +3,11 @@ from account.models import User
 
 class Chat(models.Model):
     initiator = models.ForeignKey(User,on_delete = models.CASCADE)
-    reactor = models.ForeignKey(User,on_delete = models.CASCADE)
+    reactor = models.ForeignKey(User,on_delete = models.CASCADE,related_name="reactoe")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     class Meta:
-        ordering = ('-created', '-updated')
+        ordering = ('-created_at', '-updated_at')
 
     def __str__(self):
         return self.initiator.username
@@ -15,12 +15,12 @@ class Chat(models.Model):
 class Message(models.Model):
    chat = models.ForeignKey(Chat,on_delete = models.CASCADE)
    sender = models.ForeignKey(User,on_delete = models.CASCADE)
-   receiver = models.ForeignKey(User,on_delete = models.CASCADE)
+   receiver = models.ForeignKey(User,on_delete = models.CASCADE,related_name="receiver")
    body = models.TextField(max_length=1000)
    created_at = models.DateTimeField(auto_now_add=True)
    updated_at = models.DateTimeField(auto_now=True)
    
    class Meta:
-        ordering = ('-created', '-updated')
+        ordering = ('-created_at', '-updated_at')
    def __str__(self):
         return self.sender.username
--- chat/views.py
+++ chat/views.py
@@ -6,8 +6,9 @@ from account.models import User
 
 # Create your views here.
 def index(request):
-
-    return render(request,"chat/index.html")
+    users = User.objects.all()
+    context = {'users' :users}
+    return render(request,"chat/index.html",context)
 
 
 def chat(request):
@@ -21,3 +22,26 @@ def chat(request):
         Chat.objects.create(initiator = request.user, reactor =user)
     context = {'chat': chat}
     return render(request,'chat/your_chat.html',context)
+
+def Message(request,q):
+    user = User.objects.get(id=q)
+    chat = {}
+    if user is None:
+         return HttpResponse("user you searching for, does not exist")
+    else:
+         reactor = Chat.objects.get(reactor = user) 
+         initiator = Chat.objects.get(initiator=user)
+   
+         if   reactor != None:
+             chat = reactor
+         elif initiator != None:
+            chat = initiator
+           
+    messages = Message.objects.get(chat=chat)
+    if request.method == "POST":
+          receiver = User.objects.get( id=request.POST.get('id'))
+          Message.objects.create(sender=request.user, receiver=receiver,body=request.POST.get('body') )
+          return HttpResponse('the message is sent')
+    context = {'messages':messages}
+    return render(request,'chat/your_chat.html',context)  
+        
