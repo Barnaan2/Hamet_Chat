34b6293887c8ca90c42c4c3fac6c96e33b109613
Barnaan2 <barnaantekalign@gmail.com>
2022-11-29 10:55:16 -0800

    files added

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 from . import views
-
+ 
 urlpatterns = [
     path('', views.index,name="index"),
     path('chat/',views.chat, name="chat"),
     path('message/<str:q>/ajax/',views.ajax, name ="ajax"),
     path('message/<str:q>/',views.message, name="message")
 ]
+ 
